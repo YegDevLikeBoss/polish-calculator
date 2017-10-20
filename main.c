5a504e787c63ad7298406a718c9fa181dae2ad7e
YegDevLikeBoss <31534524+YegDevLikeBoss@users.noreply.github.com>
2017-10-20 10:56:59 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,88 +1,140 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
+#include <ctype.h>
+#include <string.h>
 
-#define Treshold 100
-
-int isempty();
-int isfull();
-double peek();
-double pop();
-void push(double data);
-
-float stack[Treshold];
-int top=-1;
-
-
+void celTo(float tmp); /*Celsius converter*/
+void farTo(float tmp); /*Fahrenheit converter*/
+void kelTo(float tmp); /*Kelvin converter*/
+void universal(float tmp); /*Undefined converter*/
+void error(char type, char tp2); /*Error handling*/
 
 int main(int argc, char *argv[])
 {
-    int i;
-    double res=0.0f, valm=0.0f, val=0.0f;
+    int capScale, status;
+    float val, temp;
 
-    if (argc==1)
-        printf("Error: This program inputs data via command line arguments.\nUse:\n+ to add\n- to substract\nx(latin) to muliply\n/ to devide\n");
+    status = 1;
+    val = atof(argv[1]); /*converting pointer to float*/
 
-
-    for(i=2; i<=argc; i++)
+    if(argc==2)
     {
-        switch(*argv[i-1])
+        universal(val);
+    }
+    else if (argc==3)
+    {
+        temp = *argv[2];
+        capScale = toupper(temp); /*letter capitalization*/
+        switch(capScale) /*Scale scenario switching*/
         {
-            case '+': {val=pop()+0.0; valm=pop()+0.0; /*printf("%.2f %.2f", val, valm);*/ res=valm+val; push(res); /*printf("cc%.2fcc", peek());*/}
+            case 'C': /*In case of Celsius*/
+                {
+                    if (val > -273.16)
+                        celTo(val);
+                    else
+                    {
+                        error('b', '\0');
+                        status=0;
+                    }
+                }
             break;
-            case '-': {val=pop()+0.0; valm=pop()+0.0; res=valm-val; push(res);}
+            case 'F': /*In case of Fahrenheit*/
+                {
+                    if (val > -459.67)
+                        farTo(val);
+                    else
+                    {
+                        error('b', '\0');
+                        status=0;
+                    }
+                }
             break;
-            case 'x': {val=pop()+0.0; valm=pop()+0.0; res=valm*val; push(res);}
+            case 'K': /*In case of Kelvin*/
+                {
+                    if (val > -0.01)
+                        kelTo(val);
+                    else
+                    {
+                        error('b', '\0');
+                        status=0;
+                    }
+                }
             break;
-            case '/': {val=pop()+0.0; valm=pop()+0.0; res=valm/val; push(res);}
-            break;
-            default: {push(atof(argv[i-1])); /*printf("xx%.2fxx", peek());*/}
+            default:
+                {
+                     error('a', capScale);
+                     status=0;
+                }
         }
     }
-
-    printf("Result: %.2f\n", res);
-    return 0;
+    return status; /*Main output*/
 }
-
-int isempty()
-{
-    if (top==-1)
-        return 1;
-    else
-        return 0;
-}
-int isfull()
-{
-    if (top==Treshold)
-        return 1;
-    else
-        return 0;
-}
-double peek()
-{
-   return stack[top];
-}
-double pop()
-{
-    int data;
-
-    if(!isempty())
+void celTo(float tmp) /*Celsius conversion*/
     {
-        data = stack[top];
-        --top;
-        return data;
-    } /*else
-        {
-            printf("Could not retrieve data, Stack is empty.\n");
-        }*/
-}
-void push(double data)
-{
+        float celsius, fahrenheit, kelvin;
 
-   if(!isfull()) {
-      top++;
-      stack[top] = data;
-   } else {
-      printf("Could not insert data, Stack is full.\n");
-   }
-}
+        celsius = tmp;
+        fahrenheit = 9*celsius/5+32;
+        kelvin = celsius + 273.15;
+        printf("%.2f F\n%.2f K ;-)\n", fahrenheit, kelvin);
+    }
+void farTo(float tmp) /*Fahrenheit conversion*/
+    {
+        float celsius, fahrenheit, kelvin;
+
+        fahrenheit = tmp;
+        celsius = (fahrenheit - 32)*5/9;
+        kelvin = celsius + 273.15;
+        printf("%.2f C\n%.2f K ;-)\n", celsius, kelvin);
+    }
+void kelTo(float tmp) /*Kelvin conversion*/
+    {
+        float celsius, fahrenheit, kelvin;
+
+        kelvin = tmp;
+        celsius = kelvin - 273.15;
+        fahrenheit = 9*celsius/5+32;
+        printf("%.2f C\n%.2f F ;-)\n", celsius, fahrenheit);
+    }
+void universal(float tmp) /*undefined conversion*/
+    {
+        float celsius, fahrenheit, kelvin;
+
+        printf("For Celsius:\n");
+        if (tmp > -273.16)
+        {
+            celsius = tmp;
+            fahrenheit = 9*celsius/5+32;
+            kelvin = celsius + 273.15;
+            printf("%.2f C\n%.2f F\n%.2f K ;-)\n", celsius, fahrenheit, kelvin);
+        }
+        else printf("Invalid value of temperature :-(\n");
+        printf("For Fahrenheit:\n");
+        if (tmp > -459.67)
+        {
+            fahrenheit = tmp;
+            celsius = (fahrenheit - 32)*5/9;
+            kelvin = celsius + 273.15;
+            printf("%.2f C\n%.2f F\n%.2f K ;-)\n", celsius, fahrenheit, kelvin);
+        }
+        else printf("Invalid value of temperature :-(\n");
+        printf("For Kelvin:\n");
+        if (tmp > -0.01)
+        {
+            kelvin = tmp;
+            celsius = kelvin - 273.15;
+            fahrenheit = 9*celsius/5+32;
+            printf("%.2f C\n%.2f F\n%.2f K ;-)\n", celsius, fahrenheit, kelvin);
+        }
+        else printf("Invalid value of temperature :-(\n");
+    }
+void error(char type, char tp2) /*Error handling*/
+    {
+        switch(type)
+        {
+            case 'a':printf("Incompatible argument: '%c'. Try 'c', 'f' or 'k' instead :-(\n", tp2);
+            break;
+            case 'b':printf("Invalid value of temperature :-(\n");
+            break;
+        }
+    }
